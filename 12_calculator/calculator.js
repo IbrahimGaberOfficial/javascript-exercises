cf243fa54832b951833e3ccdd056fdb9b6248789
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-12-08 00:01:38 +0200

    Finish `12_calculator`

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a = 0, b = 0) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a = 0, b = 0) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr = []) {
+  // return arr.reduce((sum, num)=> (sum + num), 0); // corrent one
+  let sum = 0;
+  arr.forEach((item, inex, arr) => (sum += item));
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr = []) {
+  return arr.reduce((result, num) => result * num, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = num == 0 ? 1 : num;
+  while (--num > 0) {
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +36,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
