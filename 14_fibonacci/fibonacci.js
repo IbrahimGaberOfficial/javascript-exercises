3bbee07a4169ca6e7ac2986c20a93e765a93bfb7
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-12-08 00:52:33 +0200

    finish `14_fibonacci`.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+  let first = 1,
+    second = 1,
+    result = nth == 0 ? 0 : 1;
+  result = nth < 0 ? "OOPS" : result;
+  
+  for (let i = 3; i <= nth; i++) {
+    result = first + second;
+    first = second;
+    second = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
