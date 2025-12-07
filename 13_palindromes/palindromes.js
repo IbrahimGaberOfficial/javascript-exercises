d4fff947f2f3dca3caf7eb6c5adc92a6598d632c
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-12-08 00:04:24 +0200

    finish `13_pallindromes` using two pointers.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str = "") {
+  str = str.toLowerCase();
+  let isChar = function (char) {
+    if (
+      (char >= "a" && char <= "z") ||
+      (char >= "A" && char <= "Z") ||
+      (char >= "0" && char <= "9")
+    )
+      return true;
+    return false;
+  };
+  for (let l = 0, r = str.length - 1; l < r; ) {
+    if (!isChar(str[l])) {
+      l++;
+      continue;
+    } else if (!isChar(str[r])) {
+      r--;
+      continue;
+    }
 
+    if (str[l] !== str[r]) return false;
+    l++;
+    r--;
+  }
+  return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// palindromes('ZZZZ car, a man, a maracaz.')
